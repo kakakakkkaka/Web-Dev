a6511a2a9ceea4139b0ca8de907c87e0f509e230
kakakakkkaka <CJCJCJfromgrovestreet@protonmail.com>
2026-03-18 03:33:37 +0500

    Add files via upload

---
+++ lab7/task1/informatics/ConditionalStatements/2practiceC.py
@@ -0,0 +1,13 @@
+x = int(input())
+y = int(input())
+
+if x == 1:
+    if y == 1:
+        print("YES")
+    else:
+        print("NO")
+else:
+    if y != 1:
+        print("YES")
+    else:
+        print("NO")
